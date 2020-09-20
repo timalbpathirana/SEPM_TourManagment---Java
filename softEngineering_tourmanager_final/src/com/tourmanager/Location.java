7c0e6e5ef1ba46b91fbfa92c047b32ff4f4e7f4a
Timal Pathirana <s3703235@student.rmit.edu.au>
2020-09-20 14:35:15 +1000

    Committing changes for add location error fix

---
+++ softEngineering_tourmanager_final/src/com/tourmanager/Location.java
@@ -0,0 +1,77 @@
+package com.tourmanager;
+
+public class Location {
+    private Integer id;
+    private String name;
+    private Double Xco;
+    private Double YCo;
+    private String desc;
+    private Double time;
+    private static int nextID = 1;
+
+    public Location(String name, Double xco, Double YCo, String desc, Double time) {
+        this.name = name;
+        this.Xco = xco;
+        this.YCo = YCo;
+        this.desc = desc;
+        this.time = time;
+        this.id = nextID++;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getXco() {
+        return Xco;
+    }
+
+    public void setXco(Double xco) {
+        Xco = xco;
+    }
+
+    public Double getYCo() {
+        return YCo;
+    }
+
+    public void setYCo(Double YCo) {
+        this.YCo = YCo;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public Double getTime() {
+        return time;
+    }
+
+    public void setTime(Double time) {
+        this.time = time;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Location{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", Xco=" + Xco +
+                ", YCo=" + YCo +
+                ", desc='" + desc + '\'' +
+                ", time=" + time +
+                '}';
+    }
+}
